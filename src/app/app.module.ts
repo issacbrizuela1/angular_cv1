72f017042654e979a43c1bcbdebce15d6d37fdf9
Issac <74998947+issacbrizuela1@users.noreply.github.com>
2023-01-08 22:23:57 -0600

    update main comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './main/home/home.component';
+import { NavbarComponent } from './assets/navbar/navbar.component';
+import { SkillsComponent } from './main/skills/skills.component';
+import { ProyectsComponent } from './main/proyects/proyects.component';
+import { PersonalproyectsComponent } from './main/personalproyects/personalproyects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SkillsComponent,
+    ProyectsComponent,
+    PersonalproyectsComponent
   ],
   imports: [
     BrowserModule,
